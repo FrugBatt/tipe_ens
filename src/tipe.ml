45352f6fc0952b7ffbb9193616b6b2e1ca923cf9
Hugoooo_ <frug.batt.dev@gmail.com>
2022-06-13 23:04:32 +0200

    Fix algorithm 3 & Erdos-Gallai theoreme check

--- src/tipe.ml
+++ src/tipe.ml
@@ -1,5 +1,6 @@
 open Tas;;
 open Printf;;
+open Tri;;
 
 (* On implémente un graphe à l'aide de sa matrice d'adjacence *)
 type graphe = bool array array;;
@@ -91,29 +92,100 @@ let print_graph_degree (g : graphe) =
 ;;
 
 
+let take_last tab i =
+  let n = Array.length tab in
+  if i < 0 || n = 0 then 0
+  else if i >= n then tab.(n-1)
+  else tab.(i)
+;;
+
+(* Erdos Gallai check *)
+let erdos_gallai (k : int array) =
+  let n = Array.length k and tab = Array.of_list (tri_fusion (Array.to_list k)) in
+  let up_sum = Array.make (n+1) 0 and down_sum = Array.make (n+2) [||] in
+  for i = 1 to n do up_sum.(i) <- up_sum.(i-1) + tab.(i-1) done;
+  for p = n downto 1 do
+    down_sum.(p) <- Array.make (tab.(p-1)+1) 0;
+    for l = 0 to (tab.(p-1)) do
+      down_sum.(p).(l) <- l + (take_last down_sum.(p+1) l)
+    done;
+  done;
+  let i = ref 1 in
+  while !i <= n && (up_sum.(!i) <= !i*(!i-1) + (take_last down_sum.(!i) !i)) do incr i done;
+  (!i = n+1)
+;;
+
+(* let algorithm_3 (k : (unit -> int) array) = *)
+(*   let n = Array.length k and degs = Array.map (fun k_i -> k_i ()) k in *)
+(*   if erdos_gallai degs then printf "Graph OK\n" else printf "Graph not OK\n"; *)
+(*   let g = Array.make_matrix n n false in *)
+(*   let nodes_tab = Array.init n (fun i -> {deg= degs.(i); value= i}) in *)
+(*   let f1 = cree_tas nodes_tab and f2 = {content= Array.make n {deg= 0; value=0}; length= 0} in *)
+(*   while f1.length > 0 do *)
+(*     let node = retrait f1 and connected = ref [] in *)
+(*     while node.deg > 0 && f2.length > 0 do *)
+(*       let connect = retrait f2 in *)
+(*       g.(node.value).(connect.value) <- true; *)
+(*       g.(connect.value).(node.value) <- true; *)
+(*       node.deg <- node.deg - 1; *)
+(*       connect.deg <- connect.deg - 1; *)
+(*       if connect.deg > 0 then connected := connect::!connected *)
+(*     done; *)
+(*     List.iter (fun i -> ajout i f2) !connected; *)
+(*     if node.deg > 0 then ajout node f2 *)
+(*   done; *)
+(*   if f2.length > 0 then printf "Graphe invalid: %d\n" f2.length; *)
+(*   (g : graphe) *)
+(* ;; *)
+
+
+(* let print_tab tab = *)
+(*   let n = Array.length tab in *)
+(*   for i = 0 to (n-1) do *)
+(*     printf "%d:%d -- " tab.(i).value tab.(i).deg *)
+(*   done; *)
+(*   printf "\n" *)
+(* ;; *)
+
 (* Optimized algorithm *)
 let algorithm_3 (k : (unit -> int) array) =
   let n = Array.length k and degs = Array.map (fun k_i -> k_i ()) k in
+  if erdos_gallai degs then printf "Graph OK\n" else printf "Graph not OK\n";
   let g = Array.make_matrix n n false in
   let nodes_tab = Array.init n (fun i -> {deg= degs.(i); value= i}) in
-  let f1 = cree_tas nodes_tab and f2 = {content= Array.make n {deg= 0; value=0}; length= 0} in
+  let f1 = cree_tas nodes_tab in
   while f1.length > 0 do
-    let node = retrait f1 and connected = ref [] in
-    while node.deg > 0 && f2.length > 0 do
-      let connect = retrait f2 in
-      g.(node.value).(connect.value) <- true;
-      g.(connect.value).(node.value) <- true;
-      node.deg <- node.deg - 1;
-      connect.deg <- connect.deg - 1;
+    let node = retrait f1 and connection = ref false and connected = ref [] in
+    while node.deg > 0 && f1.length > 0 && not !connection do
+      let connect = retrait f1 in
+      if not g.(node.value).(connect.value) then begin
+        g.(node.value).(connect.value) <- true;
+        g.(connect.value).(node.value) <- true;
+        node.deg <- node.deg - 1;
+        connect.deg <- connect.deg - 1;
+        connection := true;
+        (* printf "Connection %d:%d -- %d:%d\n" node.value node.deg connect.value connect.deg; *)
+      end;
       if connect.deg > 0 then connected := connect::!connected
     done;
-    List.iter (fun i -> ajout i f2) !connected;
-    if node.deg > 0 then ajout node f2
+    (* if node.deg > 0 && f1.length = 0 then print_tab f1.content; printf "Length : %d\n" (List.length !connected); *)
+    if node.deg > 0 && f1.length = 0 then failwith "Graph invalid";
+    if node.deg > 0 then connected := node::!connected;
+    List.iter (fun i -> ajout i f1) !connected;
   done;
-  if f2.length > 0 then printf "Graphe invalid: %d" f2.length;
   (g : graphe)
 ;;
 
-let k () = 1+(Random.int 998);;
-let tab_k = Array.make 1000 k;;
-print_graph_degree (algorithm_3 tab_k);;
+
+(* let k () = (Random.int 500);; *)
+(* let tab_k = Array.make 500 k;; *)
+(* let k () = 7;; *)
+(* let tab_k = Array.make 6 k;; *)
+(* let t = [| 11 ;12 ;13 ;8 ;10 ;4 ;3 ;5 ;3 ;6 ;3 ;5 ;4 ;5 ;3 ;3 ;3 ;3 ;3 ;3|];; *)
+(* let t = [| 5 ;8 ;8 ;6 ;6 ;3 ;4 ;3 ;4 ;3 |];; *)
+(* let t = [| 3 ;5 ;4 ;2 ;2 ;2 |];; *)
+let t = [| 85 ;50 ;54 ;54 ;36 ;69 ;17 ;22 ;15 ;54 ;13 ;9 ;15 ;10 ;22 ;8 ;31 ;11 ;9 ;24 ;7 ;11 ;17 ;13 ;9 ;18 ;21 ;14 ;15 ;17 ;15 ;10 ;11 ;11 ;9 ;7 ;23 ;9 ;15 ;14 ;16 ;10 ;7 ;5 ;10 ;6 ;11 ;7 ;13 ;16 ;5 ;3 ;4 ;5 ;11 ;3 ;9 ;7 ;15 ;13 ;8 ;9 ;7 ;5 ;6 ;9 ;8 ;5 ;7 ;4 ;24 ;11 ;6 ;13 ;4 ;7 ;8 ;10 ;8 ;4 ;8 ;5 ;12 ;14 ;7 ;8 ;8 ;5 ;8 ;4 ;5 ;5 ;8 ;9 ;4 ;13 ;4 ;4 ;9 ;11 ;4 ;5 ;11 ;8 ;5 ;5 ;7 ;6 ;3 ;7 ;11 ;7 ;8 ;6 ;9 ;5 ;9 ;7 ;3 ;4 ;3 ;5 ;7 ;4 ;6 ;3 ;5 ;6 ;6 ;7 ;11 ;7 ;7 ;5 ;4 ;8 ;8 ;7 ;4 ;6 ;4 ;4 ;3 ;4 ;6 ;7 ;3 ;5 ;5 ;8 ;7 ;4 ;6 ;3 ;7 ;5 ;4 ;4 ;7 ;4 ;5 ;5 ;6 ;7 ;10 ;3 ;5 ;4 ;3 ;5 ;6 ;4 ;6 ;6 ;3 ;4 ;5 ;5 ;5 ;4 ;5 ;4 ;4 ;3 ;5 ;6 ;5 ;4 ;5 ;4 ;4 ;3 ;9 ;7 ;4 ;8 ;5 ;3 ;5 ;11 ;5 ;3 ;4 ;4 ;4 ;5 ;3 ;5 ;5 ;5 ;4 ;4 ;3 ;5 ;5 ;4 ;5 ;3 ;3 ;3 ;4 ;3 ;6 ;3 ;3 ;6 ;6 ;4 ;4 ;3 ;8 ;5 ;6 ;3 ;3 ;3 ;3 ;3 ;4 ;3 ;3 ;5 ;4 ;6 ;3 ;6 ;4 ;7 ;4 ;5 ;4 ;4 ;3 ;5 ;3 ;6 ;3 ;5 ;3 ;3 ;4 ;3 ;3 ;4 ;5 ;6 ;4 ;4 ;5 ;5 ;3 ;4 ;4 ;5 ;4 ;4 ;3 ;3 ;3 ;4 ;3 ;3 ;3 ;7 ;3 ;4 ;5 ;3 ;3 ;3 ;5 ;5 ;3 ;3 ;3 ;4 ;4 ;3 ;3 ;4 ;6 ;3 ;4 ;4 ;3 ;4 ;3 ;3 ;4 ;4 ;3 ;5 ;3 ;4 ;3 ;4 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;3 ;4 ;3 ;4 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;4 ;3 ;3 ;3 ;3 ;4 ;3 ;4 ;4 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;7 ;3 ;3 ;3 ;4 ;4 ;3 ;3 ;5 ;3 ;3 ;3 ;4 ;4 ;3 ;4 ;4 ;3 ;5 ;3 ;4 ;4 ;4 ;3 ;4 ;4 ;3 ;5 ;3 ;5 ;4 ;3 ;3 ;3 ;3 ;4 ;4 ;3 ;3 ;3 ;4 ;4 ;3 ;3 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;4 ;4 ;4 ;4 ;3 ;3 ;4 ;3 ;4 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;4 ;4 ;3 ;3 ;3 ;4 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;4 ;3 ;3 ;5 ;5 ;3 ;4 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 ;3 |];;
+let n = Array.length t;;
+let tab_k = Array.init n (fun i -> (fun () -> t.(i)));;
+let g = algorithm_3 tab_k;;
+print_graph_degree g;;
---
+++ src/tri.ml
@@ -0,0 +1,19 @@
+let rec partition = function
+  | [] -> ([],[])
+  | [a] -> ([a],[])
+  | a::b::t -> let (l1,l2) = partition t in (a::l1,b::l2)
+;;
+
+let rec fusion l1 l2 = match (l1,l2) with
+  | ([],_) -> l2
+  | (_,[]) -> l1
+  | (h1::t1,h2::t2) ->
+      if h1 > h2 then h1::(fusion t1 l2)
+      else h2::(fusion l1 t2)
+;;
+
+let rec tri_fusion = function
+  | [] -> []
+  | [a] -> [a]
+  | l -> let (l1,l2) = partition l in fusion (tri_fusion l1) (tri_fusion l2)
+;;
